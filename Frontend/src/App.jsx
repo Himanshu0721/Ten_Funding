265efee9cd36188db72b2aecc775dc8b285497b9
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-12-28 17:36:13 +0530

    Did some work on home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
 import Registrationform from "./Components/RegistrationForm/RegistrationForm";
 
@@ -8,7 +9,10 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Registrationform />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Registrationform />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </>
---
+++ Frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/form");
+  };
+
+  return (
+    <div className="home-container">
+      <div className="home-header">
+        <h1>One place to find the right investor for you.</h1>
+        <hr />
+        <div className="home-inner-div">
+          <h2>Check out our database of active investors.</h2>
+          <button onClick={handleButtonClick}>Join The Database</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
