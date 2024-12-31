9c1b072a24dab9c06d8c28eb65b1a1924d867214
Debadrita-rgb <debadritapaul76@gmail.com>
2024-12-31 20:44:19 +0530

    backend

--- backend/main_project/tan/forms.py
+++ backend/main_project/tan/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .models import TanFunding
+from .models import funding
 from django.forms import ModelForm
 class ContactForm(ModelForm):
-    name = forms.CharField(label='Your Name', max_length=100)
-    email = forms.EmailField(label='Your Email')
-    message = forms.CharField(label='Your Message', widget=forms.Textarea)
+    first_name = forms.CharField(label='Your first_name')
+    # last_name = forms.EmailField(label='Your last_name')
+    title = forms.CharField(label='Your title')
    
     
     class Meta:
-        model = TanFunding
+        model = funding
         fields ="__all__"
