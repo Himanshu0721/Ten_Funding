3fafea23686742b18c9fcb6593c281f48ff5753a
Riya Singh <riyasingh606065@gmail.com>
2025-01-13 18:38:41 +0530

    vcIndia work

---
+++ src/assets/data/aboutInfo.js
@@ -0,0 +1,38 @@
+import Image1 from "../images/fundraising_main.png";
+import Image2 from "../images/venture_main.webp";
+import Image3 from "../images/launchpad_main.jpeg";
+
+const aboutInfo = [
+  {
+    id: 1,
+    image: Image1,
+    tag: "Fundraising OS",
+    points: [
+      "One-click direct application to investor portals allows access to investor databases.",
+      "Oversee the investor pipeline at each step of the fundraising procedure.",
+      "Use effective templates and tools for managing meetings and data rooms.",
+    ],
+  },
+  {
+    id: 2,
+    image: Image2,
+    tag: "Ventures OS",
+    points: [
+      "From sourcing to finance, simplify and expedite the whole deal flow process to ensure nothing is overlooked.",
+      "Manage your network of VCs, LPs, and founders effectively to ensure you never lose contact.",
+      "Gain deep insights into your portfolio's performance and track key metrics to make data-driven investment decisions effortlessly.",
+    ],
+  },
+  {
+    id: 3,
+    image: Image3,
+    tag: "LaunchPad",
+    points: [
+      "Boost your VC career with elite domestic and international VCs.",
+      "Gain access to a database of fascinating VC prospects that is updated weekly.",
+      "Build valuable connections with top-tier venture capitalists through exclusive networking opportunities.",
+    ],
+  },
+];
+
+export default aboutInfo;
---
+++ src/assets/data/ventureInfo.js
@@ -0,0 +1,27 @@
+import Image1 from "../images/venture1.jpg";
+import Image2 from "../images/venture2.png";
+
+const ventureInfo = [
+  {
+    id: 1,
+    image: Image1,
+    tag: "Deal Flow Manager",
+    points: [
+      "Provide founders with special application linkages so that deal flow information can be centralized as it enters the pipeline.",
+      "Use personalized templates that fit your workflow preferences to better manage your pipeline.",
+      "Improve the starting evaluation process by monitoring more than 20 parameters.",
+    ],
+  },
+  {
+    id: 2,
+    image: Image2,
+    tag: "Network CRM",
+    points: [
+      "Streamline communication: Track response times, engagement frequency, and shared interests to prioritize follow-ups with founders, LPs, and VCs.",
+      "Organize your network effectively by classifying contacts based on meeting notes, last interaction date, lead quality, and more.",
+      "Enhance collaboration: Categorize connections by industry focus, investment stage, or mutual introductions to facilitate targeted discussions and joint ventures.",
+    ],
+  },
+];
+
+export default ventureInfo;
