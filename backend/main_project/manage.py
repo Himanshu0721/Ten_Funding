0ff41d4dcd23b4f6f75adad8e25e7027ef4e33e5
Debadrita-rgb <debadritapaul76@gmail.com>
2025-01-24 13:32:12 +0530

    change manage.py file

--- backend/main_project/manage.py
+++ backend/main_project/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_project.settings')
     try:
